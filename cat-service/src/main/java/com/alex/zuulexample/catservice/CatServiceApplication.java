3ee648de7494786ed6379bb48732e621222c1f0d
kiseru <kis.alex97@gmail.com>
2018-10-21 19:03:43 +0300

    Added eureka client to cat-service.

--- cat-service/src/main/java/com/alex/zuulexample/catservice/CatServiceApplication.java
+++ cat-service/src/main/java/com/alex/zuulexample/catservice/CatServiceApplication.java
@@ -2,8 +2,10 @@ package com.alex.zuulexample.catservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CatServiceApplication {
 
     public static void main(String[] args) {
